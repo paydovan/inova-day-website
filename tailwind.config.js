9d2c740e71038b3e728d703ce894c058697307b8
padovan <jpmpadovan@gmail.com>
2024-10-11 12:45:54 -0300

    creating main content and a little of the second section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       fontFamily: {
         "sans": ["Urbanist", "sans-serif"],
       },
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+        'spin-fast': 'spin 3s linear infinite'
+      }
     },
   },
   plugins: [],
